c8d8e66602b388bddecdb794ba369848a20de3ab
zawkindev <zawkindev@gmail.com>
2024-01-29 20:30:16 +0500

    feat: add tab component

---
+++ src/components/common/Tab.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-row bg-white rounded-md border overflow-hidden">
+    <div
+      v-for="(option, index) in options"
+      :class="getTabClass(index)"
+      class="flex justify-center items-center"
+      @click="activeTab = index"
+    >
+      <span class="p-4">{{ option }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps(["options"]);
+
+const activeTab = ref(0);
+
+function getTabClass(index) {
+  return {
+    "border-r border-[#E0E7FF]": index + 1 !== props.options.length,
+    "bg-primary-300 border-primary-300 text-white":
+      index === activeTab.value,
+    "border-primary-300": index === activeTab.value - 1,
+  };
+}
+</script>
--- src/playGround/PlayGround.vue
+++ src/playGround/PlayGround.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="gap-6 bg-gray-400 h-screen w-screen flex flex-col justify-center items-center">
-    <div class="flex flex-col bg-white rounded-md gap-5 justify-center items-center p-10">
+    <div class="w-full flex flex-col bg-white rounded-md gap-5 justify-center items-center p-10">
 
       <CButton>
         Yuborish
@@ -32,6 +32,31 @@
         </p>
       </CInput>
 
+      <div class="overflow-x-auto w-full">
+        <div class="table w-full">
+          <div class="table-row-group">
+            <div class="table-row bg-gray-200 font-semibold">
+              <div class="table-cell p-2">Heasdfjasdhfalkdsfader 1</div>
+              <div class="table-cell p-2">eader 2</div>
+              <div class="table-cell p-2">Header 3</div>
+            </div>
+            <div class="table-row bg-white hover:bg-gray-100">
+              <div class="table-cell p-2">sdfasdfRow 1, Cell 1</div>
+              <div class="table-cell p-2">Row 1, Cell dsfasdfasdfasdfadsf2</div>
+              <div class="table-cell p-2">Row 1, Cell 3</div>
+            </div>
+            <div class="table-row bg-white hover:bg-gray-100">
+              <div class="table-cell p-2">Row 2, Cell 1</div>
+              <div class="table-cell p-2">Row 2, Cell 2</div>
+              <div class="table-cell p-2">Row 2, Cell 3</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+     <Tab :options="['salom','xayr','alvido']" /> 
+
     </div>
   </div>
 </template>
@@ -40,4 +65,5 @@
 import CButton from "components/base/CButton.vue"
 import Badge from "../components/common/Badge.vue";
 import CInput from "../components/base/CInput.vue";
+import Tab from "components/common/Tab.vue"
 </script>
