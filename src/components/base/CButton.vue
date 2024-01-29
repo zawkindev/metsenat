629141d8131991def263219319d2ae35896eb96a
zawkindev <zawkindev@gmail.com>
2024-01-29 12:06:18 +0500

    feat: icons & button component

---
+++ src/components/base/CButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <button class="w-full py-[14px] px-6 bg-primary-300">
+    <slot name="icon"> </slot>
+    <slot name="default"> </slot>
+  </button>
+</template>
+
+<script setup></script>
