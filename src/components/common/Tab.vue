8f5eb20f13e2512d17a159bc12a059a2a6345893
zawkindev <zawkindev@gmail.com>
2024-01-29 20:52:42 +0500

    fix: tab component

--- src/components/common/Tab.vue
+++ src/components/common/Tab.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="flex flex-row bg-white rounded-md border overflow-hidden">
-    <div
-      v-for="(option, index) in options"
-      :class="getTabClass(index)"
-      class="flex justify-center items-center"
-      @click="activeTab = index"
-    >
-      <span class="p-4">{{ option }}</span>
+    <div v-for="(option, index) in options" class="flex justify-center items-center text-blue-400"
+      :class="getTabClass(index)" @click="activeTab = index">
+      <span class="p-4 uppercase" :class="{ 'text-white': index === activeTab }">{{
+        option
+      }}</span>
     </div>
   </div>
 </template>
@@ -21,9 +19,9 @@ const activeTab = ref(0);
 function getTabClass(index) {
   return {
     "border-r border-[#E0E7FF]": index + 1 !== props.options.length,
-    "bg-primary-300 border-primary-300 text-white":
-      index === activeTab.value,
+    "bg-primary-300 border-primary-300 text-white": index === activeTab.value,
     "border-primary-300": index === activeTab.value - 1,
   };
 }
 </script>
+
