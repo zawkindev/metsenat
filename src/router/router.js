7949b2cd325dec528437790349626aec81faf707
zawkindev <zawkindev@gmail.com>
2024-01-29 10:54:23 +0500

    feat: color variables

--- src/router/router.js
+++ src/router/router.js
@@ -1,43 +1,17 @@
 import VueRouter from "vue-router";
 import HomeLayout from "./layouts/HomeLayout.vue";
 import AuthLayout from "./layouts/AuthLayout.vue";
-import DashboardLayout from "./layouts/DashboardLayout.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: HomeLayout,
-    children: [
-      {
-        path: "",
-        component: () => import("./views/HomeView.vue"),
-        meta: { layout: "home" },
-      },
-    ],
-  },
-  {
-    path: "/auth",
-    name: "Auth",
-    component: AuthLayout,
-    children: [
-      {
-        path: "login",
-        component: () => import("./views/LoginView.vue"),
-        meta: { layout: "auth" },
-      },
-    ],
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: DashboardLayout,
     meta: { layout: "dashboard" },
     children: [
       {
         path: "",
-        component: () => import("./views/DashboardView.vue"),
-        meta: { layout: "dashboard" },
+        component: () => import("../views/HomeView.vue"),
       },
       {
         path: "students",
@@ -58,6 +32,18 @@ const routes = [
             name: "SponsorProfile",
           },
         ],
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    name: "Auth",
+    component: AuthLayout,
+    children: [
+      {
+        path: "login",
+        component: () => import("./views/LoginView.vue"),
+        meta: { layout: "auth" },
       },
     ],
   },
