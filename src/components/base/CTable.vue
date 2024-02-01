7d147310564f7ff6781cfffd88d88d9a21eac064
zawkindev <zawkindev@gmail.com>
2024-02-01 06:34:21 +0500

    feat: best table component

--- src/components/base/CTable.vue
+++ src/components/base/CTable.vue
@@ -8,52 +8,15 @@
         <li>
           <ul class="text-[#B1B1B8] text-left flex gap-6 px-[14px]">
             <slot name="header">
-              <li class="w-[2%] text-center">#</li>
-              <li
-                v-for="(column, index) in columns"
-                :key="index"
-                :class="`w-[${column.width}] ${index === 0 ? 'text-left' : 'text-center'}`"
-              >
-                {{ column.label }}
-              </li>
+
             </slot>
           </ul>
         </li>
 
         <div>
-          <li
-            v-for="(item, index) in data?.results"
-            :key="index"
-            class="bg-white py-[22px] px-[14px] rounded-lg my-5 border-[#B2B7C1]"
-          >
-            <ul class="flex items-center justify-between">
-              <slot name="body">
-                <li class="w-[2%] text-center">{{ index + 1 }}</li>
-                <li class="w-[34%] text-left">{{ item?.full_name }}</li>
-                <li class="w-[10%] text-center">{{ item?.phone }}</li>
-                <li class="w-[16%] text-center">
-                  {{ formatMoney(item?.spent)
-                  }}<span class="ml-2 text-gray-400">UZS</span>
-                </li>
-                <li class="w-[15%] text-center">
-                  {{ formatMoney(item?.sum) }}
-                  <span class="ml-2 text-gray-400">UZS</span>
-                </li>
-                <li class="w-[15%] text-center">
-                  {{ formatDate(item?.created_at) }}
-                </li>
-                <li class="w-[8%] text-center">
-                  <!-- <CBadge :status="item?.get_status_display"></CBadge> -->
-                  <Badge :variant="statusType[item?.get_status_display]">
-                    {{ item?.get_status_display }}
-                  </Badge>
-                </li>
-                <li class="w-[8%] text-center flex items-center justify-center">
-                  <img src="icons/eye.svg" alt="eye icon" />
-                </li>
-              </slot>
-            </ul>
-          </li>
+          <slot name="body">
+
+          </slot>
         </div>
       </ul>
     </div>
--- src/views/dashboard/SponsorsView.vue
+++ src/views/dashboard/SponsorsView.vue
@@ -1,5 +1,50 @@
 <template>
-  <CTable :data="store.sponsorsList" :columns="columns" :data-keys="dataKeys" />
+  <CTable :data="store.sponsorsList" :columns="columns" :data-keys="dataKeys">
+    <template #header>
+      <li class="w-[2%] text-center">#</li>
+      <li
+          v-for="(column, index) in columns"
+          :key="index"
+          :class="`w-[${column.width}] ${index === 0 ? 'text-left' : 'text-center'}`"
+      >
+        {{ column.label }}
+      </li>
+    </template>
+
+    <template #body>
+      <li
+            v-for="(item, index) in store.sponsorsList?.results"
+            :key="index"
+            class="bg-white py-[22px] px-[14px] rounded-lg my-5 border-[#B2B7C1]"
+          >
+            <ul class="flex items-center justify-between">
+                <li class="w-[2%] text-center">{{ index + 1 }}</li>
+                <li class="w-[34%] text-left">{{ item?.full_name }}</li>
+                <li class="w-[10%] text-center">{{ item?.phone }}</li>
+                <li class="w-[16%] text-center">
+                  {{ formatMoney(item?.spent)
+                  }}<span class="ml-2 text-gray-400">UZS</span>
+                </li>
+                <li class="w-[15%] text-center">
+                  {{ formatMoney(item?.sum) }}
+                  <span class="ml-2 text-gray-400">UZS</span>
+                </li>
+                <li class="w-[15%] text-center">
+                  {{ formatDate(item?.created_at) }}
+                </li>
+                <li class="w-[8%] text-center">
+                  <!-- <CBadge :status="item?.get_status_display"></CBadge> -->
+                  <Badge :variant="statusType[item?.get_status_display]">
+                    {{ item?.get_status_display }}
+                  </Badge>
+                </li>
+                <li class="w-[8%] text-center flex items-center justify-center">
+                  <img src="icons/eye.svg" alt="eye icon" />
+                </li>
+            </ul>
+          </li>
+    </template>
+  </CTable>
 </template>
 
 <script setup>
@@ -7,6 +52,8 @@ import { useFetch } from "@/composables/useFetch";
 import { onBeforeMount, ref } from "vue";
 import { useMetsenatStore } from "@/store/store";
 import CTable from "../../components/base/CTable.vue";
+import Badge from "~/home/zawkin/development/metsenat/src/components/common/Badge.vue";
+import {formatDate, formatMoney} from "../../utils/utils.js";
 
 const { get } = useFetch();
 const store = useMetsenatStore();
@@ -32,6 +79,13 @@ const fetchData = async (page) => {
   }
 };
 
+
+const statusType = {
+  Yangi: "primary",
+  Moderatsiyada: "warn",
+  Tasdiqlangan: "success",
+  "Bekor qilingan": "disabled",
+};
 const columns = ref([
   { label: "f.i.sh.", width: "34%" },
   { label: "Tel.Raqami", width: "16%" },
